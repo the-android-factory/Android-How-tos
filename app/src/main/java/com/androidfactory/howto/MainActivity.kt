23aec23992a2f3c96c570414d4f889e03f5da715
developer.dmp <developer.dmp@gmail.com>
2022-02-01 17:21:43 -0500

    Added Glide implementation for image loading!

--- app/src/main/java/com/androidfactory/howto/MainActivity.kt
+++ app/src/main/java/com/androidfactory/howto/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.androidfactory.howto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
+        val button: Button = findViewById(R.id.simpleDataDisplayActivityButton)
         button.setOnClickListener {
             val intent = Intent(this, SimpleDataDisplayActivity::class.java)
-            intent.putExtra("data2", "Hello from MainActivity")
+            intent.putExtra("data", "Hello from MainActivity")
             startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.glideButton).setOnClickListener {
+            startActivity(Intent(this, GlideImageLoadingActivity::class.java))
         }
     }
 }
