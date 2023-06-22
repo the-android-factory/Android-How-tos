7e5a502b94438a882311033fff0a1e08fe00b093
Marek Szulc <235991@edu.p.lodz.pl>
2023-06-22 02:43:41 +0200

    Init

--- app/src/main/java/com/androidfactory/howto/AssetsURL.java
+++ app/src/main/java/com/androidfactory/howto/AssetsURL.java
@@ -2,4 +2,5 @@ package com.androidfactory.howto;
 
 public class AssetsURL {
     static public String IMAGE_URL = "https://www.politykabezpieczenstwa.pl/img/artykuly/8/2021_03/android.jpg";
+    static public String VIDEO_URL = "";
 }
--- app/src/main/java/com/androidfactory/howto/CoilImageLoadingActivity.kt
+++ app/src/main/java/com/androidfactory/howto/CoilImageLoadingActivity.kt
@@ -12,7 +12,7 @@ class CoilImageLoadingActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coil_image_loading)
 
-        val imageUrl = "https://lh3.googleusercontent.com/uk_DffZl4yOQHwnr-B6mVg3pg_JJCynj6irOdSYkckcNrFK5n0fZ98JR8jgJKCaiDF1QYvjeW4_tGZDgyS1tl4g0Cg5WQ4xhLwGK=w600"
+        val imageUrl = AssetsURL.IMAGE_URL
         val button = findViewById<Button>(R.id.button)
         val imageView = findViewById<ImageView>(R.id.imageView)
 
